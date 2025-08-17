5f844863f513ad718fd569223bf6c14b152143cc
LightningHyperBlaze45654 <73149145+HyperBlaze456@users.noreply.github.com>
2025-08-17 16:05:56 +0900

    verify embedding shape

--- vocab_expansion/test_load.py
+++ vocab_expansion/test_load.py
@@ -20,6 +20,9 @@ def view_checkpoint(checkpoint_path: str):
     params = items['params']['params']
     print(params.keys())
 
+    embedding = params['token_embedder']
+    print(embedding.shape)
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Minimum model overview")
     parser.add_argument("--checkpoint-path", required=True,
